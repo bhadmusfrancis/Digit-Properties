482f6c9076f256c4ac14f1a8b0ac7c6cbfb8336d
bhadmusfrancis <bhadmusfrancis@yahoo.com>
2026-04-28 16:26:07 +0100

    Trends Auto pics

--- web/src/app/admin/layout.tsx
+++ web/src/app/admin/layout.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import Link from 'next/link';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth-options';
 import { USER_ROLES } from '@/lib/constants';
 import { AdminNav } from '@/components/layout/AdminNav';
 
